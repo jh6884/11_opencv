2afbca077837abc27186a145f1d9da70ddc27777
jh6884 <pumpkinalpha@gmail.com>
2025-08-11 16:04:35 +0900

    code for HOG

--- src/validation_emotion.py
+++ src/validation_emotion.py
@@ -1,34 +1,40 @@
-import cv2, dlib
+import cv2 
 import matplotlib.pyplot as plt
-import tensorboard as tf
+import dlib
+import tensorflow as tf 
 import numpy as np
 
-image = cv2.imread('../img/charles-etoroma.jpg')
+image = cv2.imread('../data/images/charles-etoroma.jpg')
+#cv2.imshow('charles', image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
-plt.figure(figsize=(8,8))
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.show()
-
-# cv2.imshow('charles', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+#plt.figure(figsize=(8,8))
+#plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+#plt.show()
 
 face_detector = dlib.cnn_face_detection_model_v1('../models/mmod_human_face_detector.dat')
-
-face_detection = face_detector(face_detector, 1)
-
-network = tf.keras.models.load.model('../img/models/emotion_model.h5')
+face_detection = face_detector(image, 1)
 
 left, top, right, bottom = face_detection[0].rect.left(), face_detection[0].rect.top(), face_detection[0].rect.right(), face_detection[0].rect.bottom()
-
-pred_probability = network.medols.image
 roi = image[top:bottom, left:right]
-
-cv2.imshow(roi)
-
 print(roi.shape)
 
-# normalize
+# Resize image
+roi = cv2.resize(roi, (48, 48))
+print(roi.shape)
+
+# Normalize
 roi = roi / 255
 
 roi = np.expand_dims(roi, axis=0)
+print(roi.shape)
+
+network = tf.keras.models.load_model('../data/models/emotion_model.h5')
+
+pred_probability = network.predict(roi)
+print(pred_probability)
+
+pred = np.argmax(pred_probability)
+print(pred)
+#print(test_dataset.class_indices)
